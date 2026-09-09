1e9b58b87a01990d3846f00de6f0dca86c48339b
GiovanniM27 <sonicgio27@gmail.com>
2026-09-09 18:06:36 -0400

    Update MoreBadCode.java

--- src/MoreBadCode.java
+++ src/MoreBadCode.java
@@ -1,4 +1,6 @@
 public class BadCode {
-    public static void main(String[] args) 
-        System.out.println("This won't compile") // missing brace, missing semicolon
+    public static void main(String[] args) {
+        System.out.println("This now compiles and is styled correctly.");
+    }
 }
+
